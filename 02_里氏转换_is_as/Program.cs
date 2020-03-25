46f42c204aff506da81d515bc228c4d377282a9d
俊华 蔡 <1032031912@qq.com>
2020-03-25 11:06:02 +0800

    完成了，is与as的操作

--- 02_里氏转换_is_as/Program.cs
+++ 02_里氏转换_is_as/Program.cs
@@ -10,12 +10,20 @@ namespace _02_里氏转换_is_as
     {
 
     }
-    //子类
+    //子类-apple
     class Apple:Friut
     {
         public void EatApple()
         {
             Console.WriteLine("我是苹果的吃法");
+        }
+    }
+    //子类-Banana
+    class Banana :Friut
+    {
+        public void EatBanana()
+        {
+            Console.WriteLine("我是香蕉的吃法");
         }
     }
 
@@ -24,7 +32,8 @@ namespace _02_里氏转换_is_as
         static void Main(string[] args)
         {
             //1）子类可以赋值给父类
-           //这里隐藏着一个东西，如果有一个地方需要父类作为参数，我们可以给一个子类代替
+            //这里隐藏着一个东西，如果有一个地方需要父类作为参数，我们可以给一个子类代替
+            //Friut fr = new Banana();
             Friut fr = new Apple();
             //object->string 
             //object->list
@@ -32,8 +41,32 @@ namespace _02_里氏转换_is_as
             //Console.WriteLine(str);
 
             //2)如果父类中装的是子类对象，那么可以将这个父类强转为子类
-            Apple ap = (Apple)fr;
-            ap.EatApple();
+            //is的做法
+            if (fr is Apple==true)
+            {
+                Apple ap = (Apple)fr;
+                ap.EatApple();
+            }
+            else
+            {
+                Console.WriteLine("is 转化失败");
+            }
+
+            //as
+            //Friut Fr = new Apple();
+            Friut Fr = new Banana();
+            Banana ba = Fr as Banana;
+            if (ba==null)
+            {
+                Console.WriteLine("as 转换失败");
+            }
+            else
+            {
+                ba.EatBanana();
+            }
+
+            
+
             Console.WriteLine();
             
 
